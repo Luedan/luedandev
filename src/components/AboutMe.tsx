5c7c5c59531d3505791eefee13b5767783c38957
Luis Atencia@Horbath <atencia17@gmail.com>
2025-09-01 08:44:41 -0500

    refactor: actualizar el estilo de fondo del componente AboutMe

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -7,7 +7,7 @@ export const AboutMe = () => {
   const aboutMe = getInformationByLanguage(lang)?.personalInfo?.aboutMe;
   const words = getWordsByLanguage(lang);
   return (
-    <div className="bg-gradient-to-r from-[#23232a] to-[#18181b] rounded-2xl p-8 shadow-lg border-l-4 border-yellow-500 mb-4">
+    <div className="bg-gray-900 rounded-2xl p-8 shadow-lg border-l-4 border-yellow-500 mb-4">
       <h2 className="text-3xl font-extrabold text-yellow-400 mb-4 tracking-wide">
         {words.aboutMe}
       </h2>
